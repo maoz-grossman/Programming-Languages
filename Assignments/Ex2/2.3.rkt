10841b79a57cff88f4c6a1dd5db2e75a0fd3f014
maoz-grossman <maozg2008@gmail.com>
2020-05-04 13:02:49 +0300

    Gitignor + Ex2.3

---
+++ Assignments/Ex2/2.3.rkt
@@ -0,0 +1,27 @@
+#lang pl
+
+(: power : Number Number -> Number)
+(define (power X pow)
+  (if (= 0 pow)
+      1
+      (* X (power X (- pow 1)))))
+
+
+(: square-list : (Listof Any) -> (Listof Number))
+(define (square-list lst)
+ (map(lambda (num)
+             (if (number? num) (power num 2)
+                  (error 'parse-sexpr "bad type of list in ~s" lst)))lst ))
+
+
+(: sum-of-squares : (Listof Any) -> Number)
+(define (sum-of-squares lst)
+  (foldl + 0(square-list lst)))
+
+
+
+;;Tests:
+(test (sum-of-squares '(0 0 0)) => 0)
+(test (sum-of-squares '(1 2 3)) => 14)
+(test (sum-of-squares '(1 2 a)) =error> "bad type of list in")
+

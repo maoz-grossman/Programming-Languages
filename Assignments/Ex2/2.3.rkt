9b48593235bb6b69913933d2ebc4af738d6b8c0d
maoz-grossman <maozg2008@gmail.com>
2020-05-07 12:47:31 +0300

    fix 2.3 foldl function

--- Assignments/Ex2/2.3.rkt
+++ Assignments/Ex2/2.3.rkt
@@ -1,24 +1,20 @@
 #lang pl
 
 (: power : Number Number -> Number)
+;;Gets two a & b and returns a^b
 (define (power X pow)
   (if (= 0 pow)
       1
       (* X (power X (- pow 1)))))
 
 
-(: square-list : (Listof Any) -> (Listof Number))
-(define (square-list lst)
-(if (null? lst)
-     (error 'parse-sexpr "bad type of list in ~s" lst)
-  (map(lambda (num)
-             (if (number? num) (power num 2)
-                  (error 'parse-sexpr "bad type of list in ~s" lst)))lst )))
-
-
 (: sum-of-squares : (Listof Any) -> Number)
+;; Gets list of elements, checks if all the element are numbers or null
+;;if that so returns the sum of their squeres (or 0 in case of null list)
 (define (sum-of-squares lst)
-  (foldl + 0(square-list lst)))
+  (foldl + 0 (map(lambda (num)
+                   (if (number? num) (power num 2)
+                       (error 'parse-sexpr "bad type of list in ~s" lst)))lst )))
 
 
 
@@ -28,6 +24,6 @@
 (test (sum-of-squares '(9)) => 81)
 (test (sum-of-squares '(-9)) => 81)
 (test (sum-of-squares '(1 2 a)) =error> "bad type of list in")
-(test (sum-of-squares '()) =error> "bad type of list in")
+(test (sum-of-squares '()) => 0)
 
 

808364e7c4ce55b4653daa59de71ce1e07d07d25
maoz-grossman <maozg2008@gmail.com>
2020-05-25 17:34:46 +0300

    fix WAE

--- Assignments/Ex3/WAE.rkt
+++ Assignments/Ex3/WAE.rkt
@@ -92,36 +92,59 @@
        eval({with {x E1} E2}) = eval(E2[eval(E1)/x])
   |#
 
- (: sqrt-to-list : Real -> Real)
- (define (sqrt-to-list num)
-   (if (negative? num)
-       (error 'sqrt-to-list "`sqrt' requires a nonnegative
-input: ~s" num)
-       (sqrt num)))
 
-  (: eval : WAE -> Real)
+
+(: bin-op : (Number Number -> Number) (Listof Number) (Listof Number) -> (Listof Number))
+;; applies a binary numeric function on all combinations of numbers from
+;; the two input lists, and return the list of all of the results
+(define (bin-op op ls rs)
+ (: helper : Number (Listof Number) -> (Listof Number))
+ (define (helper l rs);; l-is the first number of the left side list
+ (: f : Number -> Number)
+ (define (f elemnt_of_rs)
+   (op l elemnt_of_rs))
+ (map f rs));;Every iteration we send a number from the right side list
+ (if (null? ls) null
+ (append (helper (first ls) rs) (bin-op op (rest ls) rs))))
+
+
+
+(: sqrt+ : (Listof Number) -> (Listof Number))
+;; a version of `sqrt' that takes a list of numbers, and return a list
+;; with twice the elements, holding the two roots of each of the inputs;
+;; throws an error if any input is negative.
+(define (sqrt+ ns)
+ (cond [(null? ns) ns]
+ [(negative? (first ns)) (error 'sqrt-to-list "`sqrt' requires a nonnegative
+input: ~s" (first ns))]
+ [else (let ([val (sqrt (first ns))]) (append (list val (* -1 val)) (sqrt+ (rest ns))))])) 
+
+
+
+  (: eval : WAE -> (Listof Number))
   ;; evaluates WAE expressions by reducing them to numbers
   (define (eval expr)
     (cases expr
-      [(Num n) n]
-      [(Add l r) (+ (eval l) (eval r))]
-      [(Sub l r) (- (eval l) (eval r))]
-      [(Mul l r) (* (eval l) (eval r))]
-      [(Div l r) (/ (eval l) (eval r))]
-      [(Sqrt e) ( sqrt-to-list (eval e))]
+      [(Num n) (list n)]
+      [(Add l r) ( bin-op  +  (eval l)  (eval r))]
+      [(Sub l r) ( bin-op  -  (eval l)  (eval r))]
+      [(Mul l r) ( bin-op  *  (eval l)  (eval r))]
+      [(Div l r) ( bin-op  /  (eval l)  (eval r))]
+      [(Sqrt e) (sqrt+ (eval e))] 
       [(With bound-id named-expr bound-body)
        (eval (subst bound-body
                     bound-id
-                    (Num (eval named-expr))))]
+                    (Num ( first(eval named-expr)))))]
       [(Id name) (error 'eval "free identifier: ~s" name)]))
 
-  (: run : String -> Number)
+  (: run : String -> (Listof Number))
   ;; evaluate a WAE program contained in a string
   (define (run str)
     (eval (parse str)))
 
   ;; tests
-  (test (run "5") => 5)
+  #|
+  (test (run "5") => '(5))
   (test (run "{+ 5 5}") => 10)
   (test (run "{with {x {+ 5 5}} {+ x x}}") => 20)
   (test (run "{with {x {+ 5 5}} {* x x}}") => 100)
@@ -143,6 +166,21 @@ input")
 (test (run "{with {x {+ 5 20}} {sqrt x}}") => 5)
 (test (run "{with {6 {+ 5 5}} {+ x x}}") =error> "bad `with' syntax in")
 (test (run "{with {x {> 5 5}} {+ x x}}") =error> "bad syntax in")
+|#
+(test (run "{sqrt 9}") => '(3 -3))
+(test (run "{sqrt 1}") => '(1 -1))
+(test (run "{sqrt 0}") => '(0 0))
+(test (run "{with {x {+ 5 20}} {sqrt x}}") => '(5 -5))
+(test (run "{sqrt -1}") =error> "`sqrt' requires a nonnegative
+input")
+(test (run "{with {x 5} {+ x {with {y 3} x}}}") => '(10))
+(test (run "{+ {sqrt 1} 3}") => '(4 2))
+(test (run "{+ {/ {+ {sqrt 1} 3} 2} {sqrt 100}}")
+ => '(12 -8 11 -9))
+(test (run "{sqrt {+ 16 {* {+ 1 {sqrt 1}} {/ 9 2}}}}")
+ => '(5 -5 4 -4))
+
+
 
 
 #|

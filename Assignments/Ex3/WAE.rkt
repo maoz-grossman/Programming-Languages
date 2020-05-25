77c25cc7948de4d26d3012f649070e305f738d4f
maoz-grossman <maozg2008@gmail.com>
2020-05-25 18:45:21 +0300

    :+1:  Ex3.a is done

--- Assignments/Ex3/WAE.rkt
+++ Assignments/Ex3/WAE.rkt
@@ -1,7 +1,7 @@
   #lang pl
 
   #| BNF for the WAE language:
-       <WAE> ::= <num>
+       <WAE> ::= (Listof <num>)
                | { + <WAE> <WAE> }
                | { - <WAE> <WAE> }
                | { * <WAE> <WAE> }
@@ -13,7 +13,7 @@
 
   ;; WAE abstract syntax trees
   (define-type WAE
-    [Num  Number]
+    [Num  (Listof Number)]
     [Add  WAE WAE]
     [Sub  WAE WAE]
     [Mul  WAE WAE]
@@ -26,7 +26,7 @@
   ;; to convert s-expressions into WAEs
   (define (parse-sexpr sexpr)
     (match sexpr
-      [(number: n)    (Num n)]
+      [(number: n) (Num (list n))]
       [(symbol: name) (Id name)]
       [(cons 'with more)
        (match sexpr
@@ -125,7 +125,7 @@ input: ~s" (first ns))]
   ;; evaluates WAE expressions by reducing them to numbers
   (define (eval expr)
     (cases expr
-      [(Num n) (list n)]
+      [(Num n)  n]
       [(Add l r) ( bin-op  +  (eval l)  (eval r))]
       [(Sub l r) ( bin-op  -  (eval l)  (eval r))]
       [(Mul l r) ( bin-op  *  (eval l)  (eval r))]
@@ -134,7 +134,7 @@ input: ~s" (first ns))]
       [(With bound-id named-expr bound-body)
        (eval (subst bound-body
                     bound-id
-                    (Num ( first(eval named-expr)))))]
+                    (Num (eval named-expr))))]
       [(Id name) (error 'eval "free identifier: ~s" name)]))
 
   (: run : String -> (Listof Number))
@@ -143,30 +143,7 @@ input: ~s" (first ns))]
     (eval (parse str)))
 
   ;; tests
-  #|
-  (test (run "5") => '(5))
-  (test (run "{+ 5 5}") => 10)
-  (test (run "{with {x {+ 5 5}} {+ x x}}") => 20)
-  (test (run "{with {x {+ 5 5}} {* x x}}") => 100)
-  (test (run "{with {x {+ 3 5}} {/ x 2}}") => 4)
-  (test (run "{with {x 5} {+ x x}}") => 10)
-  (test (run "{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}}") => 14)
-  (test (run "{with {x 5} {with {y {- x 3}} {+ y y}}}") => 4)
-  (test (run "{with {x 5} {+ x {with {x 3} 10}}}") => 15)
-  (test (run "{with {x 5} {+ x {with {x 3} x}}}") => 8)
-  (test (run "{with {x 5} {+ x {with {y 3} x}}}") => 10)
-  (test (run "{with {x 5} {with {y x} y}}") => 5)
-  (test (run "{with {x 5} {with {x x} x}}") => 5)
-  (test (run "{with {x 1} y}") =error> "free identifier")
-  (test (run "{sqrt 9}") => 3)
-  (test (run "{sqrt 1}") => 1)
-(test (run "{sqrt 0}") => 0)
-(test (run "{sqrt -1}") =error> "`sqrt' requires a nonnegative
-input")
-(test (run "{with {x {+ 5 20}} {sqrt x}}") => 5)
-(test (run "{with {6 {+ 5 5}} {+ x x}}") =error> "bad `with' syntax in")
-(test (run "{with {x {> 5 5}} {+ x x}}") =error> "bad syntax in")
-|#
+(test (run "{with {x {+ 5 20}} {sqrt x}}") => '(5 -5))
 (test (run "{sqrt 9}") => '(3 -3))
 (test (run "{sqrt 1}") => '(1 -1))
 (test (run "{sqrt 0}") => '(0 0))
@@ -179,8 +156,24 @@ input")
  => '(12 -8 11 -9))
 (test (run "{sqrt {+ 16 {* {+ 1 {sqrt 1}} {/ 9 2}}}}")
  => '(5 -5 4 -4))
-
-
+(test (run "{with {6 {+ 5 5}} {+ x x}}") =error> "bad `with' syntax in")
+(test (run "{with {x {> 5 5}} {+ x x}}") =error> "bad syntax in")
+(test (run "{with {x {+ 5 20}}  {+ 11 {sqrt x}} }") => '(16 6))
+(test (run "{with {x {+ 7 2}}  {+ {sqrt 16} {sqrt x}} }") => '(7 1 -1 -7))
+(test (run "5") => '(5))
+  (test (run "{+ 5 5}") => '(10))
+  (test (run "{with {x {+ 5 5}} {+ x x}}") => '(20))
+  (test (run "{with {x {+ 5 5}} {* x x}}") => '(100))
+  (test (run "{with {x {+ 3 5}} {/ x 2}}") => '(4))
+  (test (run "{with {x 5} {+ x x}}") => '(10))
+  (test (run "{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}}") => '(14))
+  (test (run "{with {x 5} {with {y {- x 3}} {+ y y}}}") => '(4))
+  (test (run "{with {x 5} {+ x {with {x 3} 10}}}") => '(15))
+  (test (run "{with {x 5} {+ x {with {x 3} x}}}") => '(8))
+  (test (run "{with {x 5} {+ x {with {y 3} x}}}") => '(10))
+  (test (run "{with {x 5} {with {y x} y}}") => '(5))
+  (test (run "{with {x 5} {with {x x} x}}") => '(5))
+  (test (run "{with {x 1} y}") =error> "free identifier")
 
 
 #|
